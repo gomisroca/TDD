1b79f48d29088362962fa0d08fac3059315e387a
asfalfa <glkin9@gmail.com>
2024-10-17 11:42:08 +0200

    parallel change: step 2

--- part1/src/prices.mjs
+++ part1/src/prices.mjs
@@ -19,8 +19,8 @@ function createApp(database) {
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
     const date = parseDate(req.query.date);
-    const date2 = parseDate(req.query.date);
-    const cost = calculateCost(age, type, date, baseCost);
+    const date2 = parsePlainDate(req.query.date);
+    const cost = calculateCost(age, type, date2, baseCost);
     res.json({ cost });
   });
 
